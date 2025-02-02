4882dfb5501610357d744df43d0f7507259f5318
idris10215 <mohammedidris10215@gmail.com>
2025-02-02 23:26:45 +0530

    Updated the files

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,14 @@
-const Button = () => {
+const Button = ({ name, isBeam = false, containerClass}) => {
     return (
-        <div>Button</div>
+        <button className={`btn $${containerClass}`} >
+            {isBeam && (
+                <span className="relative flex h-3 w-3">
+                    <span className="btn-ping" />
+                    <span className="btn-ping_dot" />
+                </span>
+            )}
+            {name}
+        </button>
     )
 }
 export default Button
